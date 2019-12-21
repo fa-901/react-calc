83539f792174698796048f32108953fdcd06ecf0
Farhan Alam <farhan_w901@outlook.com>
2019-12-21 20:27:19 +0600

    added new packages & output display
    bootstrap & jquery afdded

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
+		this.state = {
+			output: '0',
+		}
 	}
 	render() {
+		let { output } = this.state;
 		return (
-			<div>
-				Hi
+			<div className='calc-box container p-2'>
+				<textarea rows={2} className={`form-control`}>
+					{output}
+				</textarea>
 			</div>
 		);
 	}
