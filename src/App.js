e9370d78391cc7a19f844abe3b56e5f378b1324f
Farhan Alam <farhan_w901@outlook.com>
2019-12-21 22:38:11 +0600

    something

--- src/App.js
+++ src/App.js
@@ -9,11 +9,45 @@ class App extends Component {
 	}
 	render() {
 		let { output } = this.state;
+
+		var btnArr = [
+			{ label: 'C', value: '', class: '', click: '' },
+			{ label: '<-', value: '', class: '', click: '' },
+			{ label: '/', value: '', class: '', click: '' },
+			{ label: '7', value: '', class: '', click: '' },
+			{ label: '8', value: '', class: '', click: '' },
+			{ label: '9', value: '', class: '', click: '' },
+			{ label: '*', value: '', class: '', click: '' },
+			{ label: '4', value: '', class: '', click: '' },
+			{ label: '5', value: '', class: '', click: '' },
+			{ label: '6', value: '', class: '', click: '' },
+			{ label: '-', value: '', class: '', click: '' },
+			{ label: '1', value: '', class: '', click: '' },
+			{ label: '2', value: '', class: '', click: '' },
+			{ label: '3', value: '', class: '', click: '' },
+			{ label: '+', value: '', class: '', click: '' },
+			{ label: '=', value: '', class: '', click: '' },
+		];
+		var btns = btnArr.map((val,i)=>{
+			return(
+				<div key={`${val}${i}`}>
+					{val.label}
+				</div>
+			)
+		});
+
 		return (
 			<div className='calc-box container p-2'>
-				<textarea rows={2} className={`form-control`}>
-					{output}
-				</textarea>
+				<textarea rows={2} className={`form-control mb-1`} value={output}/>
+				{btns}
+				<div className='row'>
+					<div className='col-sm-3'>
+						<button className='btn btn-primary'>3</button>
+					</div>
+					<div className='col-sm-3'>
+						<button className='btn btn-primary fas fa-hiking'></button>
+					</div>
+				</div>
 			</div>
 		);
 	}
