d881b3ffd8ccae46ab8ec4a58092e2fe789de640
Farhan Alam <farhan_w901@outlook.com>
2019-12-22 10:36:27 +0600

    added grid

--- src/App.js
+++ src/App.js
@@ -11,42 +11,40 @@ class App extends Component {
 		let { output } = this.state;
 
 		var btnArr = [
-			{ label: 'C', value: '', class: '', click: '' },
-			{ label: '<-', value: '', class: '', click: '' },
-			{ label: '/', value: '', class: '', click: '' },
-			{ label: '7', value: '', class: '', click: '' },
-			{ label: '8', value: '', class: '', click: '' },
-			{ label: '9', value: '', class: '', click: '' },
-			{ label: '*', value: '', class: '', click: '' },
-			{ label: '4', value: '', class: '', click: '' },
-			{ label: '5', value: '', class: '', click: '' },
-			{ label: '6', value: '', class: '', click: '' },
-			{ label: '-', value: '', class: '', click: '' },
-			{ label: '1', value: '', class: '', click: '' },
-			{ label: '2', value: '', class: '', click: '' },
-			{ label: '3', value: '', class: '', click: '' },
-			{ label: '+', value: '', class: '', click: '' },
-			{ label: '=', value: '', class: '', click: '' },
+			{ value: 'C', label: 'C', class: '', click: '' },
+			{ value: '<-', label: '<-', class: '', click: '' },
+			{ value: '/', label: '/', class: '', click: '' },
+			{ value: '*', label: '*', class: '', click: '' },
+			{ value: '7', label: '7', class: '', click: '' },
+			{ value: '8', label: '8', class: '', click: '' },
+			{ value: '9', label: '9', class: '', click: '' },
+			{ value: '-', label: '-', class: '', click: '' },
+			{ value: '4', label: '4', class: '', click: '' },
+			{ value: '5', label: '5', class: '', click: '' },
+			{ value: '6', label: '6', class: '', click: '' },
+			{ value: '+', label: '+', class: '', click: '' },
+			{ value: '1', label: '1', class: '', click: '' },
+			{ value: '2', label: '2', class: '', click: '' },
+			{ value: '3', label: '3', class: '', click: '' },
+			{ value: '=', label: '=', class: 'grid2xrow', click: '' },
+			{ value: '0', label: '0', class: 'grid2xcol', click: '' },
+			{ value: '.', label: '.', class: '', click: '' },
 		];
 		var btns = btnArr.map((val,i)=>{
 			return(
-				<div key={`${val}${i}`}>
-					{val.label}
-				</div>
+				// <div >
+				<button key={`${val}${i}`} className={`${val.class}`}>
+						{val.label}
+					</button>
+				// </div>
 			)
 		});
 
 		return (
 			<div className='calc-box container p-2'>
 				<textarea rows={2} className={`form-control mb-1`} value={output}/>
-				{btns}
-				<div className='row'>
-					<div className='col-sm-3'>
-						<button className='btn btn-primary'>3</button>
-					</div>
-					<div className='col-sm-3'>
-						<button className='btn btn-primary fas fa-hiking'></button>
-					</div>
+				<div className='calc-grid'>
+					{btns}
 				</div>
 			</div>
 		);
