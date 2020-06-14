07018044a51acbf9f37d489a3774185c12006b62
Farhan Alam <farhan_w901@outlook.com>
2020-06-14 19:33:04 +0600

    Updates

---
+++ precache-manifest.5bbaca1d501e163acb10f3c46c338947.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1885161ebc866459ee14e3a1e3a5d33",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "b209b23d36770018b615",
+    "url": "/react-calc/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "38cd03a7906d09c8a205",
+    "url": "/react-calc/static/css/main.5403501a.chunk.css"
+  },
+  {
+    "revision": "b209b23d36770018b615",
+    "url": "/react-calc/static/js/2.e9208c2c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calc/static/js/2.e9208c2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38cd03a7906d09c8a205",
+    "url": "/react-calc/static/js/main.b3e9c5eb.chunk.js"
+  },
+  {
+    "revision": "3e8c4408b97a78c4f90a",
+    "url": "/react-calc/static/js/runtime-main.f452340a.js"
+  }
+]);
